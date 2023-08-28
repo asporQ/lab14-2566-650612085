627a9422922b0e86a5b8746261d1d321f13ff3d2
asporQ <120199524+asporQ@users.noreply.github.com>
2023-08-28 15:48:10 +0700

    firstrequire

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,37 @@
 "use client";
-import { Container, Text, Title } from "@mantine/core";
+import {
+  Button,
+  Container,
+  Rating,
+  Text,
+  Textarea,
+  Title,
+} from "@mantine/core";
 
 export default function FoodReviewPage() {
   return (
     <Container size="600px">
       <Title order={2}>Food Review 🍕</Title>
+      <Title order={4} mt="sm">
+        Your rating
+      </Title>
+
+      <Rating size="lg"></Rating>
+
+      <Textarea
+        label="Your review"
+        placeholder="Do you enjoy eating?"
+        minRows={3}
+        mt="xs"
+      ></Textarea>
+
+      <Button color="orange" mt="xs">
+        {" "}
+        Submit Review
+      </Button>
 
       <Text align="center" color="dimmed" my="sm">
-        Copyright © 2023 Chayanin Suatap 650610560
+        Copyright © 2023 Tikomporn Luxsanaprokin 650612085
       </Text>
     </Container>
   );
